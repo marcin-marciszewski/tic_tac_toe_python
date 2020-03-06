077040274685bbfb46fe3d98423d2a0cc6cf2922
szantilas87 <szantilas@gmail.com>
2020-03-06 12:28:24 +0000

    Place maker on the grid

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,7 @@
 game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board):
-        def player_input():
+    def player_input():
         player1 = input("Please pick a marker 'X' or 'O':> ")
         while player1.lower() != 'o' or player1.lower() != 'x':
             if player1.lower() == 'o' or player1.lower() == 'x':
@@ -10,7 +10,11 @@ def display_board(board):
                 player1 = input("It has to be 'X' or 'O':> ")
         return player1.upper()
 
-    player1 = player_input()
+        player1 = player_input()
+
+
+    def place_marker(board, marker, position):
+        board[position] = marker
 
 
     grid = [f'     |     |     \n  {board[1]}  |  {board[2]}  |  {board[3]}  \n     |     |     \n_________________\n     |     |     \n  {board[4]}  |  {board[5]}  |  {board[6]}  \n     |     |     \n_________________\n     |     |     \n  {board[7]}  |  {board[8]}  |  {board[9]}  \n     |     |     \n']
