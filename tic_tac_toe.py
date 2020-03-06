6f385057653eb93f72aef53308780e04577c9b62
szantilas87 <szantilas@gmail.com>
2020-03-06 12:25:16 +0000

    Take player input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,19 @@
 game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board):
+        def player_input():
+        player1 = input("Please pick a marker 'X' or 'O':> ")
+        while player1.lower() != 'o' or player1.lower() != 'x':
+            if player1.lower() == 'o' or player1.lower() == 'x':
+                break
+            else:
+                player1 = input("It has to be 'X' or 'O':> ")
+        return player1.upper()
+
+    player1 = player_input()
 
 
     grid = [f'     |     |     \n  {board[1]}  |  {board[2]}  |  {board[3]}  \n     |     |     \n_________________\n     |     |     \n  {board[4]}  |  {board[5]}  |  {board[6]}  \n     |     |     \n_________________\n     |     |     \n  {board[7]}  |  {board[8]}  |  {board[9]}  \n     |     |     \n']
-
     print(grid[0])
 
 
