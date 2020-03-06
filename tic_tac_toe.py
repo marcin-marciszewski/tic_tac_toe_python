ab04bf8553a318c2e3abd874c5d48401764af411
szantilas87 <szantilas@gmail.com>
2020-03-06 12:29:35 +0000

    Check win combination

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,6 +16,17 @@ def display_board(board):
     def place_marker(board, marker, position):
         board[position] = marker
 
+    def win_check(board, mark):
+        for i in range(1, len(board)):
+            if (i == 1 or i == 4 or i == 7) and (board[i] == mark and board[i+1] == mark and board[i+2] == mark):
+                return True
+            elif (i == 1 or i == 2 or i == 3) and (board[i] == mark and board[i+3] == mark and board[i+6] == mark):
+                return True
+            elif (board[1] == mark and board[5] == mark and board[9] == mark) or (board[3] == mark and board[5] == mark and board[7] == mark):
+                return True
+            else:
+                return False
+
 
     grid = [f'     |     |     \n  {board[1]}  |  {board[2]}  |  {board[3]}  \n     |     |     \n_________________\n     |     |     \n  {board[4]}  |  {board[5]}  |  {board[6]}  \n     |     |     \n_________________\n     |     |     \n  {board[7]}  |  {board[8]}  |  {board[9]}  \n     |     |     \n']
     print(grid[0])
